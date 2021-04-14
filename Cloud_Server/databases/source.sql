6289b2d799e98d018667a6c5c61711588e4db400
Oishimaru <crinsonflame@hotmail.com>
2021-04-14 12:05:08 -0400

    Database Update

--- Cloud_Server/databases/source.sql
+++ Cloud_Server/databases/source.sql
@@ -8,17 +8,46 @@ CREATE TABLE BOATS
 (
     id INTEGER NOT NULL AUTO_INCREMENT,
     boat_name TEXT,
-    identifier TEXT NOT NULL UNIQUE
+    st BIT NOT NULL, -- 0 Disabled, 1 Enabled
+
+    PRIMARY KEY(id)
 );
+
+CREATE TABLE USERS
+{
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    username TEXT NOT NULL, --@username
+    usertype INTEGER NOT NULL, -- 1 Viewer, 2 Supervisor, 3 Admin, 4 Super Admin
+    st BIT NOT NULL, --0 DISABLED, 1 ENABLED
+
+    PRIMARY KEY(id)
+};
 
 CREATE TABLE JOURNEYS
 {
     id INTEGER NOT NULL AUTO_INCREMENT,
-    journey_start DATETIME NOT NULL,
-    journey_end DATETIME NOT NULL,
+    journey_st DATETIME NOT NULL,
+    journey_ed DATETIME NOT NULL,
+    start_user INTEGER NOT NULL,
+    end_user INTEGER NOT NULL,
+    boat_id INTEGER NOT NULL,
 
-    PRIMARY KEY(id)
-}
+    PRIMARY KEY(id),
+
+    CONSTRAINT FK_BOAT_ID
+        FOREIGN KEY (boat_id)
+        REFERENCES BOATS (id),
+
+    CONSTRAINT FK_ST_U
+        FOREIGN KEY (start_user)
+        REFERENCES USERS (id),
+
+    CONSTRAINT FK_ED_U
+        FOREIGN KEY (end_user)
+        REFERENCES USERS (id)
+};
+
+
 
 CREATE TABLE HISTORICS
 (
@@ -27,10 +56,10 @@ CREATE TABLE HISTORICS
     journey_id INT NOT NULL,
     container_status TEXT NOT NULL, --'OPEN' OR 'CLOSED'
     container_weight FLOAT NOT NULL, --KG
-    boat_location TEXT NOT NULL, --LAT, LONG? gOOGLE COMPATIBLE
-    dt DATETIME NOT NULL,
-    fl_name TEXT NOT NULL,
-    reg DATETIME NOT NULL,
+    b_location TEXT NOT NULL, --LAT, LONG? gOOGLE COMPATIBLE
+    fl_name TEXT NOT NULL, --filename
+    dt DATETIME NOT NULL, --date of event
+    reg DATETIME NOT NULL, --date of data reception
     
 
     PRIMARY KEY(id),
@@ -42,20 +71,22 @@ CREATE TABLE HISTORICS
 
     CONSTRAINT FK_BOAT
         FOREIGN KEY (boat_id)
-        REFERENCES BOATS (identifier)
+        REFERENCES BOATS (id)
 );
 
 CREATE TABLE FILES
-{
+(
     id INTEGER NOT NULL AUTO_INCREMENT,
     fl_name TEXT NOT NULL,
     fl_type TEXT NOT NULL,
     fl_path TEXT NOT NULL,
     fl_url TEXT NOT NULL,
+    journey_id INTEGER NOT NULL,
+    boat_id INTEGER NOT NULL,
+    cam INTEGER NOT NULL,
     rl INT,
     dt DATETIME NOT NULL,
     reg DATETIME NOT NULL,
-    journey_id INT NOT NULL,
 
     PRIMARY KEY(id),
 
@@ -64,8 +95,13 @@ CREATE TABLE FILES
     CONSTRAINT FK_JOURNEY
         FOREIGN KEY (journey_id) 
         REFERENCES JOURNEYS (id)
+            ON DELETE CASCADE,
+
+    CONSTRAINT FK_BOAT
+        FOREIGN KEY (boat_id)
+        REFERENCES BOATS (id)
             ON DELETE CASCADE
-}
+);
 
 CREATE USER 'orbittas'@'localhost' IDENTIFIED WITH mysql_native_password BY '#B04tTr4ck3r++';
 
