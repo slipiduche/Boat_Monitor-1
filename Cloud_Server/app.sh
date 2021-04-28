36f11e2be3d0c02f57a6bb267f52d64abaa129e4
Oishimaru <crinsonflame@hotmail.com>
2021-04-28 19:11:00 -0400

    update files

---
+++ Cloud_Server/app.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget -O app.js https://raw.githubusercontent.com/Oishimaru/Boat_Monitor/master/Cloud_Server/app.js
---
+++ Cloud_Server/source.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget -O ./databases/source.sql https://raw.githubusercontent.com/Oishimaru/Boat_Monitor/master/Cloud_Server/databases/source.sql
