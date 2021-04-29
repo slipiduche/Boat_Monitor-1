6e4ed8e3ba1042111d9a5e6ea3100c2d548bc15f
Oishimaru <crinsonflame@hotmail.com>
2021-04-29 10:00:34 -0400

    He

--- Cloud_Server/chash.js
+++ Cloud_Server/chash.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 
-const plainText = 'p<RhA7#X_LWBB(O_0&<a,D/,f#2")7B+';
+const plainText = '';
 
 const hash = '$2b$10$tKgldLwEBI7vjXKcKWue/Of636wzx0xwJ/BsywDERHS2bwodNO1R6';
 
--- Cloud_Server/hash.js
+++ Cloud_Server/hash.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-const plainText = 'p<RhA7#X_LWBB(O_0&<a,D/,f#2")7B+';
+const plainText = '';
 
 
 bcrypt.hash(plainText, saltRounds)
