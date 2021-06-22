3eae65f1ad6c2b2b09833d66365dec16008358d7
Oishimaru <crinsonflame@hotmail.com>
2021-06-22 11:49:05 -0400

    sh

---
+++ Boat_Device/modules.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+wget -O ./modules/com.py https://raw.githubusercontent.com/Oishimaru/Boat_Monitor/master/Boat_Device/modules/com.py
+
+wget -O ./modules/disk.py https://raw.githubusercontent.com/Oishimaru/Boat_Monitor/master/Boat_Device/modules/disk.py
+
+wget -O ./modules/file.py https://raw.githubusercontent.com/Oishimaru/Boat_Monitor/master/Boat_Device/modules/file.py
+
+wget -O ./modules/imaging.py https://raw.githubusercontent.com/Oishimaru/Boat_Monitor/master/Boat_Device/modules/imaging.py
+
+wget -O ./modules/IO.py https://raw.githubusercontent.com/Oishimaru/Boat_Monitor/master/Boat_Device/modules/IO.py
+
+wget -O ./modules/location.py https://raw.githubusercontent.com/Oishimaru/Boat_Monitor/master/Boat_Device/modules/location.py
+
+wget -O ./modules/mqtts.py https://raw.githubusercontent.com/Oishimaru/Boat_Monitor/master/Boat_Device/modules/mqtts.py
+
+wget -O ./modules/network.py https://raw.githubusercontent.com/Oishimaru/Boat_Monitor/master/Boat_Device/modules/network.py
+
+wget -O ./modules/one_wire.py https://raw.githubusercontent.com/Oishimaru/Boat_Monitor/master/Boat_Device/modules/one_wire.py
+
+wget -O ./modules/params.py https://raw.githubusercontent.com/Oishimaru/Boat_Monitor/master/Boat_Device/modules/params.py
+
+wget -O ./modules/weight.py https://raw.githubusercontent.com/Oishimaru/Boat_Monitor/master/Boat_Device/modules/weight.py
---
+++ Boat_Device/program.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget -O program.py https://raw.githubusercontent.com/Oishimaru/Boat_Monitor/master/Boat_Device/program.py
+
+wget -O test.py https://raw.githubusercontent.com/Oishimaru/Boat_Monitor/master/Boat_Device/test.py
+
