45a029717923faee37dbef9b3495e3457c0bc5c4
Oishimaru <crinsonflame@hotmail.com>
2021-06-22 11:39:47 -0400

    cat

---
+++ Boat_Device/pip.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+pip3 install opencv-python
+
+pip3 install pyserial
+
+pip3 install Rpi.GPIO
+
+pip3 install gpiozero
+
+pip3 install w1thermsensor
+
+pip3 install hx711
+
+pip3 install paho-mqtt
