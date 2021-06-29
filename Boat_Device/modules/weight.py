9935480b8dd6b622105649ac0212bb3525ee0623
Oishimaru <crinsonflame@hotmail.com>
2021-06-29 12:19:15 -0400

    iewfn

--- Boat_Device/modules/weight.py
+++ Boat_Device/modules/weight.py
@@ -32,12 +32,20 @@ def getWeight(samples):
 
     raw = 0;
 
+    GPIO.setmode(GPIO.BCM)
+    
     try:     
         
         hx711.reset()   # Before we start, reset the HX711 (not obligate)
         
         raw  = hx711.get_raw_data(readings =samples);
-   
+    
+    except Exception as e:
+
+        print("Weith Measurement Exception: ");
+
+        print(e);
+
     finally:
 
         GPIO.cleanup()  # always do a GPIO cleanup in your scripts!
